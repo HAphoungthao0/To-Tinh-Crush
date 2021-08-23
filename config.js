95fbf68363d13aed6540d79653995f6bde8a98e0
HAphoungthao0 <89231404+HAphoungthao0@users.noreply.github.com>
2021-08-23 09:44:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng tui đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng tui đã say ngay từ lúc thấy nụ cười ấy? ',
+    desc: 'Phải chăng tui đã say ngay từ lúc thấy nụ cười ấy?? ',
     btnYes: 'Vẫn cứ là yêu thương <33',
     btnNo: 'Không nha :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại thưn tuii zậy??? <3',
