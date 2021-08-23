83d97505d9884d3a3f1123fad1d58802df015451
HAphoungthao0 <89231404+HAphoungthao0@users.noreply.github.com>
2021-08-23 09:38:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Bạn ơi bạn à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao bạn cứ đi nhầm đường
-    Lạc vào tim tui lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+                Mà sao bạn cứ đi nhầm đường
+                Lạc vào tim tui lẻ loi
+                Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng tui đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng tui đã say ngay từ lúc thấy nụ cười ấy ',
+    desc: 'Phải chăng tui đã say ngay từ lúc thấy nụ cười ấy? ',
     btnYes: 'Vẫn cứ là yêu thương <33',
-    btnNo: 'Không, bạn iu à :3',
+    btnNo: 'Không nha :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại thưn tuii zậy <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100005043886917'
 } 
